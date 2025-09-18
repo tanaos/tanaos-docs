db57650fddf42cac56c1af2d7cdc3f1acb97afe5
rlucatoor <rlucato95@gmail.com>
2025-09-18 18:26:49 +0200

    add announcement bar

--- consts.ts
+++ consts.ts
@@ -2,6 +2,8 @@
 export const WEBSITE_BASE_URL: string = 'https://tanaos.com';
 export const PLATFORM_BASE_URL: string = 'https://platform.tanaos.com';
 export const DOCS_BASE_URL: string = 'https://docs.tanaos.com';
+export const ARTIFEX_GITHUB_URL: string = 'https://github.com/tanaos/artifex';
+export const SYNTHEX_GITHUB_URL: string = 'https://github.com/tanaos/synthex';
 
 // Synthex
 export const SYNTHEX_INTRO_URL: string = '/synthex/intro';
